1a40389478245e16d86a206b02a561272390c35e
DianaMariaG <dianamariag002@gmail.com>
2022-01-28 17:25:07 +0200

    second commit

--- src/test/java/ro/fasttrackit/curs15/homework/TemperatureConvertorTest.java
+++ src/test/java/ro/fasttrackit/curs15/homework/TemperatureConvertorTest.java
@@ -8,14 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TemperatureConvertorTest {
     @Test
     @DisplayName("WHEN temperature in Fahrenheit THEN return temperature in Celsius")
-    void testConversion(){
+    void testGetCelsius() {
         //SETUP
         TemperatureConvertor temperatureConvertor = new TemperatureConvertor();
         //RUN
         double actual1 = temperatureConvertor.fahrenheitToCelsius(14);
-        double actual2 = temperatureConvertor.celsiusToFahrenheit(10);
         //ASSERT
         assertThat(actual1).isEqualTo(-10);
+    }
+
+    @Test
+    @DisplayName("WHEN temperature in Celsius THEN return temperature in Fahrenheit")
+    void testGetFahrenheit() {
+        //SETUP
+        TemperatureConvertor temperatureConvertor = new TemperatureConvertor();
+        //RUN
+        double actual2 = temperatureConvertor.celsiusToFahrenheit(10);
+        //ASSERT
         assertThat(actual2).isEqualTo(50);
     }
 }
